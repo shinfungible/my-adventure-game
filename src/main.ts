85071e43c67d03830b0480a6bb042453ad92718c
keishino00 <shin.enginee@gmail.com>
2023-03-15 07:13:14 +0900

    feat: 画面サイズの変更

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,13 @@ app.whenReady().then(() => {
     webPreferences: {
       preload: path.resolve(__dirname, "preload.js"),
     },
+    width: 1414,
+    height: 1000,
+    frame: false,
   });
 
   mainWindow.loadFile("dist/index.html");
+  mainWindow.setAspectRatio(1414 / 1000);
   // mainWindow.webContents.openDevTools({ mode: 'detach' });
 });
 
