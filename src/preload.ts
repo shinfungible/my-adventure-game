ad55eb8e2d36fb27fe50418347461c34f6437511
keishino00 <shin.enginee@gmail.com>
2023-03-15 20:49:44 +0900

    feat: 微調整

--- src/preload.ts
+++ src/preload.ts
@@ -1,1 +1,9 @@
-console.log("preloaded!");
+import { GameLogicType } from "./types/GameLogic";
+
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  loadClass: async (gameLogicType: GameLogicType, sceneId: string) => {
+    return await ipcRenderer.invoke('load-class', gameLogicType, sceneId);
+  },
+});
